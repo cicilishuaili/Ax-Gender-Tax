0c8a4dcebccb5ff73014aad7b7e2d4d4e5fd1705
cicilishuaili <email.sli1990@gmail.com>
2018-07-11 11:33:42 -0700

    use boto instead

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
+from boto.s3.connection import S3Connection
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = os.environ.get['SECRET_KEY']
+SECRET_KEY = S3Connection(os.environ['SECRET_KEY'])
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = S3Connection(os.environ['DATABASE_URL'])
