8d846fc4154c6bef74373f85038c15d015126454
cicilishuaili <email.sli1990@gmail.com>
2018-07-11 16:45:10 -0700

    customized to app

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
-#from boto.s3.connection import S3Connection
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = os.environ['SECRET_KEY']
-#SECRET_KEY = S3Connection(os.environ['SECRET_KEY'])
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
