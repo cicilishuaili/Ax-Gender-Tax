5d7cfcbcbd35f474ea0d7c23020fd8fb31c84919
cicilishuaili <email.sli1990@gmail.com>
2018-07-11 11:41:17 -0700

    try w/ no boto

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = os.environ.get['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 #SECRET_KEY = S3Connection(os.environ['SECRET_KEY'])
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
